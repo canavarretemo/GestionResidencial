7a59d7eec6da9a8246d27e7e742db33f18d2b971
canavarretemo <andres.navarrete89@icloud.com>
2023-06-28 19:43:25 -0500

    inicio proyecto

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, {Request, Response } from 'express'
+import logger from './utils/logger' 
+import routes from './api/routes'
+
+const app = express()
+const port = 8087
+
+
+// Crea un middleware para convertir 
+// todos los bodies de los request en JSON
+app.use(express.json())
+app.use('/api/v1', routes)
+
+app.listen(port, () =>{
+    logger.info(`Server is listening on port ${port}`)
+}
+)
